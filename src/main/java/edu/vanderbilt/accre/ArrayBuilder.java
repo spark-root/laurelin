3365bdb0b16c8dcf05a25f4f1eb35d4b94bd7631
Jim Pivarski <jpivarski@gmail.com>
2019-03-12 19:20:58 +0100

    work in progress on ArrayBuilder

--- src/main/java/edu/vanderbilt/accre/ArrayBuilder.java
+++ src/main/java/edu/vanderbilt/accre/ArrayBuilder.java
@@ -1,5 +1,7 @@
 package edu.vanderbilt.accre;
 
+import java.lang.String;
+import java.lang.IllegalArgumentException;
 import java.lang.UnsupportedOperationException;
 
 import edu.vanderbilt.accre.array.Array;
@@ -16,15 +18,47 @@ public class ArrayBuilder {
         this.interpretation = interpretation;
         this.basketEntryOffsets = basketEntryOffsets;
         this.basketNumBytes = basketNumBytes;
+
+        // TODO: check for len(basketEntryOffsets) == len(basketNumBytes) + 1
+        // TODO: check for basketEntryOffsets monotonically increasing
+        // TODO: check for both being non-negative
     }
 
-    static public interface Basket {
-        RawArray get(int i);
+    static public interface GetBasket {
+        int border(int i);
+        RawArray data(int i);
     }
 
+    public Array build(long entrystart, long entrystop) {
+        int basketstart = this.basketstart(entrystart);
+        int basketstop = this.basketstop(entrystop);
+
+        
 
 
 
+
+        
+        throw new UnsupportedOperationException("not yet finished");
+    }
+
+    private int basketstart(long entrystart) {
+        for (int i = 0;  i < this.basketEntryOffsets.length - 1;  i++) {
+            if (this.basketEntryOffsets[i] <= entrystart) {
+                return i;
+            }
+        }
+        throw new IllegalArgumentException(String.format("entrystart %ld not in branch", entrystart));
+    }
+
+    private int basketstop(long entrystop) {
+        for (int i = this.basketEntryOffsets.length - 1;  i > 0;  i--) {
+            if (entrystop <= this.basketEntryOffsets[i]) {
+                return i;
+            }
+        }
+        throw new IllegalArgumentException(String.format("entrystop %ld not in branch", entrystop));
+    }
 
 
 
