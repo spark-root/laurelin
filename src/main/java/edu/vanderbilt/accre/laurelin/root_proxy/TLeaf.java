c729e1ec1254673ab8481c9831e3aaf826355adb
Andrew Melo <andrew.m.melo@vanderbilt.edu>
2019-04-24 08:59:37 -0500

    Cleanup unneccessary casts/whitespace
    Some eclipse-requested whitespace/unnecccessary cast changes

--- src/main/java/edu/vanderbilt/accre/laurelin/root_proxy/TLeaf.java
+++ src/main/java/edu/vanderbilt/accre/laurelin/root_proxy/TLeaf.java
@@ -17,42 +17,42 @@ public class TLeaf extends TBranch {
 	@Override
 	public SimpleType getSimpleType() {
 		/*
-		 * An attempt to decode all the different ROOT type systems to 
-		 * an easy descriptor 
+		 * An attempt to decode all the different ROOT type systems to
+		 * an easy descriptor
 		 */
 		SimpleType ret = null;
-		
+
 		// fLen needs to be zero
 		switch (data.className) {
 		case ("TLeafO"):
-			ret = (SimpleType) SimpleType.Bool;
+			ret = SimpleType.Bool;
 			break;
 		case ("TLeafB"):
-			ret = (SimpleType) SimpleType.Int8;
+			ret = SimpleType.Int8;
 			break;
 		case ("TLeafS"):
-			ret = (SimpleType) SimpleType.Int16;
+			ret = SimpleType.Int16;
 			break;
 		case ("TLeafI"):
-			ret = (SimpleType) SimpleType.Int32;
+			ret = SimpleType.Int32;
 			break;
 		case ("TLeafL"):
-			ret = (SimpleType) SimpleType.Int64;
+			ret = SimpleType.Int64;
 			break;
 		case ("TLeafF"):
-			ret = (SimpleType) SimpleType.Float32;
+			ret = SimpleType.Float32;
 			break;
 		case ("TLeafD"):
-			ret = (SimpleType) SimpleType.Float64;
+			ret = SimpleType.Float64;
 			break;
 		}
-		
+
 		// Do I later want to separate fixed and not-fixed arrays?
 		if ( ((int) data.getScalar("fLen").getVal() > 1) ||
 				(data.getProxy("fLeafCount").getDataSize() != 0)){
-			ret = (SimpleType) new SimpleType.ArrayType(ret);
+			ret = new SimpleType.ArrayType(ret);
 		}
-	
+
 		if (ret == null) {
 			throw new RuntimeException("Unknown TLeaf type. Class name: " + data.className);
 		}
