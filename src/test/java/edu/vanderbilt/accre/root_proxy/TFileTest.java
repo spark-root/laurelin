38784253d76e7b9fb50f2c2b4f096b803e991938
Andrew Melo <andrew.m.melo@vanderbilt.edu>
2019-04-24 07:26:20 -0500

    Add test against NANOAOD file
    Exercise file reading with a more realistic file than the one found in
    uproot

--- src/test/java/edu/vanderbilt/accre/root_proxy/TFileTest.java
+++ src/test/java/edu/vanderbilt/accre/root_proxy/TFileTest.java
@@ -23,6 +23,13 @@ public class TFileTest {
 		tree.iterate(new String[] {"N"});
 	}
 	
+	@Test
+	public void testOpenNanoAOD() throws IOException {
+		TFile testfile = TFile.getFromFile("testdata/nano_tree.root");
+		Proxy events = testfile.getProxy("Events");
+		TTree tree = new TTree(events, testfile);
+	}
+
 	@Test(expected = NoSuchElementException.class)
 	public void testFail() throws IOException {
 		TFile testfile = TFile.getFromFile("testdata/nano_tree.root");
