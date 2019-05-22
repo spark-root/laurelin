bc19ff18c9efebd73a2dc8d7c937442952c28ae4
Andrew Melo <andrew.m.melo@vanderbilt.edu>
2019-05-21 23:17:21 -0500

    Add float4 support to column vector

--- src/main/java/edu/vanderbilt/accre/laurelin/spark_ttree/TTreeColumnVector.java
+++ src/main/java/edu/vanderbilt/accre/laurelin/spark_ttree/TTreeColumnVector.java
@@ -9,163 +9,176 @@ import org.apache.spark.sql.vectorized.ColumnarArray;
 import org.apache.spark.sql.vectorized.ColumnarMap;
 import org.apache.spark.unsafe.types.UTF8String;
 
+import edu.vanderbilt.accre.ArrayBuilder;
+import edu.vanderbilt.accre.laurelin.array.Array;
+import edu.vanderbilt.accre.laurelin.interpretation.AsDtype;
 import edu.vanderbilt.accre.laurelin.root_proxy.TBasket;
-import edu.vanderbilt.accre.laurelin.root_proxy.TTree;
+import edu.vanderbilt.accre.laurelin.root_proxy.TBranch;
 
 public class TTreeColumnVector extends ColumnVector {
-	List<TBasket> baskets;
-	
-	protected TTreeColumnVector(DataType type, List<TBasket> baskets) {
-		super(type);
-		this.baskets = baskets;
-		// TODO Auto-generated constructor stub
-	}
+    private TBranch branch;
+    private int basketIndex = 0;
+    private long [] basketEntryOffsets;
 
-	@Override
-	public void close() {
-		// TODO Auto-generated method stub
-		
-	}
+    public TTreeColumnVector(DataType type, TBranch branch) {
+        super(type);
+        this.branch = branch;
 
-	@Override
-	public boolean hasNull() {
-		// TODO Auto-generated method stub
-		return false;
-	}
+        List<TBasket> baskets = branch.getBaskets();
+    }
 
-	@Override
-	public int numNulls() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
+    @Override
+    public void close() {
+        // TODO Auto-generated method stub
 
-	@Override
-	public boolean isNullAt(int rowId) {
-		// TODO Auto-generated method stub
-		return false;
-	}
+    }
 
-	@Override
-	public boolean getBoolean(int rowId) {
-		// TODO Auto-generated method stub
-		return false;
-	}
+    @Override
+    public boolean hasNull() {
+        // TODO Auto-generated method stub
+        return false;
+    }
 
-	@Override
-	public byte getByte(int rowId) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
+    @Override
+    public int numNulls() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
 
-	@Override
-	public short getShort(int rowId) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
+    @Override
+    public boolean isNullAt(int rowId) {
+        // TODO Auto-generated method stub
+        return false;
+    }
 
-	@Override
-	public int getInt(int rowId) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
+    @Override
+    public boolean getBoolean(int rowId) {
+        // TODO Auto-generated method stub
+        return false;
+    }
 
-	@Override
-	public long getLong(int rowId) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
+    @Override
+    public byte getByte(int rowId) {
+        // TODO Auto-generated method stub
+        return 0;
+    }
 
-	@Override
-	public float getFloat(int rowId) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
+    @Override
+    public short getShort(int rowId) {
+        // TODO Auto-generated method stub
+        return 0;
+    }
 
-	@Override
-	public double getDouble(int rowId) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
+    @Override
+    public int getInt(int rowId) {
+        // TODO Auto-generated method stub
+        return 0;
+    }
 
-	@Override
-	public ColumnarArray getArray(int rowId) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    @Override
+    public long getLong(int rowId) {
+        // TODO Auto-generated method stub
+        return 0;
+    }
 
-	@Override
-	public ColumnarMap getMap(int ordinal) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    @Override
+    public float getFloat(int rowId) {
+        // Very obviously not what we want to do long-term. We should keep the
+        // underlying Array cached as long as possible
+        ArrayBuilder.GetBasket getbasket = branch.getArrayBranchCallback();
+        basketEntryOffsets = branch.getBasketEntryOffsets();
+        AsDtype asdtype = new AsDtype(AsDtype.Dtype.FLOAT4);
+        ArrayBuilder builder = new ArrayBuilder(getbasket, asdtype, basketEntryOffsets);
+        Array test = builder.build(rowId, rowId + 1);
+        float []testarray = (float [])test.toArray();
+        return testarray[0];
+    }
 
-	@Override
-	public Decimal getDecimal(int rowId, int precision, int scale) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    @Override
+    public double getDouble(int rowId) {
+        // TODO Auto-generated method stub
+        return 0;
+    }
 
-	@Override
-	public UTF8String getUTF8String(int rowId) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    @Override
+    public ColumnarArray getArray(int rowId) {
+        // TODO Auto-generated method stub
+        return null;
+    }
 
-	@Override
-	public byte[] getBinary(int rowId) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    @Override
+    public ColumnarMap getMap(int ordinal) {
+        // TODO Auto-generated method stub
+        return null;
+    }
 
-	@Override
-	protected ColumnVector getChild(int ordinal) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-	
-	/*
-	 * Return 
-	 */
+    @Override
+    public Decimal getDecimal(int rowId, int precision, int scale) {
+        // TODO Auto-generated method stub
+        return null;
+    }
 
-	@Override
-	public boolean[] getBooleans(int rowId, int count) {
-		// TODO Auto-generated method stub
-		return super.getBooleans(rowId, count);
-	}
+    @Override
+    public UTF8String getUTF8String(int rowId) {
+        // TODO Auto-generated method stub
+        return null;
+    }
 
-	@Override
-	public byte[] getBytes(int rowId, int count) {
-		// TODO Auto-generated method stub
-		return super.getBytes(rowId, count);
-	}
+    @Override
+    public byte[] getBinary(int rowId) {
+        // TODO Auto-generated method stub
+        return null;
+    }
 
-	@Override
-	public short[] getShorts(int rowId, int count) {
-		// TODO Auto-generated method stub
-		return super.getShorts(rowId, count);
-	}
+    @Override
+    protected ColumnVector getChild(int ordinal) {
+        // TODO Auto-generated method stub
+        return null;
+    }
 
-	@Override
-	public int[] getInts(int rowId, int count) {
-		// TODO Auto-generated method stub
-		return super.getInts(rowId, count);
-	}
+    /*
+     * Return
+     */
 
-	@Override
-	public long[] getLongs(int rowId, int count) {
-		// TODO Auto-generated method stub
-		return super.getLongs(rowId, count);
-	}
+    @Override
+    public boolean[] getBooleans(int rowId, int count) {
+        // TODO Auto-generated method stub
+        return super.getBooleans(rowId, count);
+    }
 
-	@Override
-	public float[] getFloats(int rowId, int count) {
-		// TODO Auto-generated method stub
-		return super.getFloats(rowId, count);
-	}
+    @Override
+    public byte[] getBytes(int rowId, int count) {
+        // TODO Auto-generated method stub
+        return super.getBytes(rowId, count);
+    }
 
-	@Override
-	public double[] getDoubles(int rowId, int count) {
-		// TODO Auto-generated method stub
-		return super.getDoubles(rowId, count);
-	}	
+    @Override
+    public short[] getShorts(int rowId, int count) {
+        // TODO Auto-generated method stub
+        return super.getShorts(rowId, count);
+    }
+
+    @Override
+    public int[] getInts(int rowId, int count) {
+        // TODO Auto-generated method stub
+        return super.getInts(rowId, count);
+    }
+
+    @Override
+    public long[] getLongs(int rowId, int count) {
+        // TODO Auto-generated method stub
+        return super.getLongs(rowId, count);
+    }
+
+    @Override
+    public float[] getFloats(int rowId, int count) {
+        // TODO Auto-generated method stub
+        return super.getFloats(rowId, count);
+    }
+
+    @Override
+    public double[] getDoubles(int rowId, int count) {
+        // TODO Auto-generated method stub
+        return super.getDoubles(rowId, count);
+    }
 }
