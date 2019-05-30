e792698b35a831f93e7b7729d3649520617e7800
Andrew Melo <andrew.m.melo@vanderbilt.edu>
2019-05-30 13:42:52 -0500

    Make Cache serializable
    To transmit the state of our reader to the executors, it needs to be
    serializable. There's currently no state, but eventually we'll want to
    pass down params like cache size, etc..

--- src/main/java/edu/vanderbilt/accre/laurelin/Cache.java
+++ src/main/java/edu/vanderbilt/accre/laurelin/Cache.java
@@ -1,9 +1,16 @@
 package edu.vanderbilt.accre.laurelin;
 
+import java.io.Serializable;
+
 import edu.vanderbilt.accre.laurelin.array.RawArray;
 import edu.vanderbilt.accre.laurelin.root_proxy.TFile;
 
-public class Cache {
+public class Cache implements Serializable {
+
+    /**
+     * Some heiroglyphics required for Serializable
+     */
+    private static final long serialVersionUID = 2935538030757378235L;
 
     public RawArray get(TFile backingFile, int last) {
         // TODO Auto-generated method stub
