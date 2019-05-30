8ac63c17b60c56c10df9caf09b6a71fda9db9d3c
Andrew Melo <andrew.m.melo@vanderbilt.edu>
2019-05-30 14:16:01 -0500

    Add WeakHashMap cache implementation
    This isn't optimal (want to add an LRU on top to hold strong references
    to the most recently accessed objects), but it's good enough for now.
    
    Fixes #14

--- src/main/java/edu/vanderbilt/accre/laurelin/Cache.java
+++ src/main/java/edu/vanderbilt/accre/laurelin/Cache.java
@@ -1,6 +1,8 @@
 package edu.vanderbilt.accre.laurelin;
 
 import java.io.Serializable;
+import java.lang.ref.WeakReference;
+import java.util.WeakHashMap;
 
 import edu.vanderbilt.accre.laurelin.array.RawArray;
 import edu.vanderbilt.accre.laurelin.root_proxy.TFile;
@@ -12,14 +14,32 @@ public class Cache implements Serializable {
      */
     private static final long serialVersionUID = 2935538030757378235L;
 
-    public RawArray get(TFile backingFile, int last) {
-        // TODO Auto-generated method stub
-        return null;
+    WeakHashMap<TFile, WeakHashMap<Integer, WeakReference<RawArray>>> cache;
+
+    public Cache() {
+        cache = new WeakHashMap<TFile, WeakHashMap<Integer, WeakReference<RawArray>>>();
     }
 
-    public void put(TFile backingFile, int last, RawArray data) {
-        // TODO Auto-generated method stub
+    public RawArray get(TFile backingFile, int last) {
+        RawArray ret = null;
+        WeakHashMap<Integer, WeakReference<RawArray>> fileMap = cache.get(backingFile);
+        if (fileMap == null) {
+            return null;
+        }
+        ret = fileMap.get(last).get();
+        return ret;
+    }
 
+    public RawArray put(TFile backingFile, int last, RawArray data) {
+        WeakHashMap<Integer, WeakReference<RawArray>> fileMap = null;
+        while (fileMap == null) {
+            fileMap = cache.get(backingFile);
+            if (fileMap == null) {
+                cache.putIfAbsent(backingFile, new WeakHashMap<Integer, WeakReference<RawArray>>());
+            }
+        }
+        fileMap.put(last, new WeakReference<RawArray>(data));
+        return data;
     }
 
 }
