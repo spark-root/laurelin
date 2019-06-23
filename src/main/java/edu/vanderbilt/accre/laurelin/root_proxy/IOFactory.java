091740c2ca204da34db5356438ea173ae5bf8c0d
Andrew Melo <andrew.m.melo@vanderbilt.edu>
2019-06-22 22:50:25 -0500

    Convert directories into files
    If the user passes in a directory to be read, expand it into a list of
    files by doing a recursive search

--- src/main/java/edu/vanderbilt/accre/laurelin/root_proxy/IOFactory.java
+++ src/main/java/edu/vanderbilt/accre/laurelin/root_proxy/IOFactory.java
@@ -5,17 +5,28 @@
 package edu.vanderbilt.accre.laurelin.root_proxy;
 
 import java.io.IOException;
+import java.util.List;
 import java.util.regex.Pattern;
 
 public class IOFactory {
+    static final String hadoopPattern = "^[a-zA-Z]+:/.*";
+
     public static FileInterface openForRead(String path) throws IOException {
         /**
          * Depending on the incoming path, load an implementation
          */
-        if (Pattern.matches("^[a-zA-Z]+:/.*", path)) {
+        if (Pattern.matches(hadoopPattern, path)) {
             return new HadoopFile(path);
         } else {
             return new NIOFile(path);
+        }
+    }
+
+    public static List<String> expandPathToList(String path) throws IOException {
+        if (Pattern.matches(hadoopPattern,  path)) {
+            return HadoopFile.expandPathToList(path);
+        } else {
+            return NIOFile.expandPathToList(path);
         }
     }
 }
