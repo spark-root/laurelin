17d79fcf0f57a2faf1b2ba2985cb985958019351
Andrew Melo <andrew.m.melo@vanderbilt.edu>
2019-06-26 12:40:09 -0500

    Tidy up comment

--- src/main/java/edu/vanderbilt/accre/laurelin/interpretation/AsDtype.java
+++ src/main/java/edu/vanderbilt/accre/laurelin/interpretation/AsDtype.java
@@ -267,7 +267,7 @@ public class AsDtype implements Interpretation {
                 /*
                  * index:  0  1  2  3  4  5  6  7  8  9
                  * src:   11 22 33 44
-                 * dest:  00 00 11 22 00 00 33 44
+                 * dest:  00 00 00 00 11 22 33 44
                  */
                 converted = ByteBuffer.allocate(source.length() * 2);
                 for (int i = 0; i < source.length(); i += 4) {
