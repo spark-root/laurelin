68dbe8009ee9610c1a392554cfaec5cec49ae4bc
Andrew Melo <andrew.m.melo@vanderbilt.edu>
2019-07-28 21:33:52 -0500

    Ensure overridePristineReads is false
    This test case will fail if someone mistakenly sets the config value to
    true

---
+++ src/test/java/edu/vanderbilt/accre/HelperTest.java
@@ -0,0 +1,9 @@
+package edu.vanderbilt.accre;
+
+import static org.junit.Assert.assertFalse;
+
+public class HelperTest {
+    public void test() {
+        assertFalse(Helpers.overridePristineReads);
+    }
+}
