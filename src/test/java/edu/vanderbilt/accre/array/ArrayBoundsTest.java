d632562b7efc04f18732a003a6601174a7ab9051
Andrew Melo <andrew.m.melo@vanderbilt.edu>
2019-08-23 17:40:26 -0500

    Revert "JaggedArrayPrep.clip was also wrong; fixed it and added a test that reads all Jet_pt from nano_19.root"
    This reverts commit 5ba53857c065c0d80889e7fd575639556bf71f18.

--- src/test/java/edu/vanderbilt/accre/array/ArrayBoundsTest.java
+++ src/test/java/edu/vanderbilt/accre/array/ArrayBoundsTest.java
@@ -8,76 +8,65 @@ import java.util.List;
 
 import org.junit.Test;
 
-import edu.vanderbilt.accre.laurelin.Cache;
-import edu.vanderbilt.accre.laurelin.array.ArrayBuilder;
-import edu.vanderbilt.accre.laurelin.array.JaggedArrayPrep;
-import edu.vanderbilt.accre.laurelin.array.RawArray;
-import edu.vanderbilt.accre.laurelin.interpretation.AsDtype;
-import edu.vanderbilt.accre.laurelin.interpretation.AsJagged;
-import edu.vanderbilt.accre.laurelin.interpretation.Interpretation;
-import edu.vanderbilt.accre.laurelin.root_proxy.TBasket;
-import edu.vanderbilt.accre.laurelin.root_proxy.TBranch;
-import edu.vanderbilt.accre.laurelin.root_proxy.TFile;
-import edu.vanderbilt.accre.laurelin.root_proxy.TTree;
-import edu.vanderbilt.accre.laurelin.spark_ttree.SlimTBranch;
-
 public class ArrayBoundsTest {
 
-    @Test
-    public void testNanoBaskets() {
-        long[] basketEntryOffsets = {0, 1000, 4266, 8532};
-        class DummyBranchCallback implements ArrayBuilder.GetBasket {
-            @Override
-            public ArrayBuilder.BasketKey basketkey(int basketid) {
-                int keylen = 0;
-                int last = (int) (2 * (basketEntryOffsets[basketid + 1] - basketEntryOffsets[basketid]));
-                int objlen = last;
-                return new ArrayBuilder.BasketKey(keylen, last, objlen);
-            }
 
-            @Override
-            public RawArray dataWithoutKey(int basketid) {
-                return new RawArray(ByteBuffer.allocate((int) (2 * (basketEntryOffsets[basketid + 1] - basketEntryOffsets[basketid]))));
-            }
-        }
 
-        Interpretation interpretation = new AsDtype(AsDtype.Dtype.UINT2);
-        ArrayBuilder builder0 = new ArrayBuilder(new DummyBranchCallback(), interpretation, basketEntryOffsets, null, 0, 1000);
-        ArrayBuilder builder1 = new ArrayBuilder(new DummyBranchCallback(), interpretation, basketEntryOffsets, null, 1000, 4266);
-    }
 
-    private TTree getTestTree() throws IOException {
-        String testPath = "testdata/nano_19.root";
-        String testTree = "Events";
-        File f = new File(testPath);
-        TFile currFile = TFile.getFromFile(testPath);
-        return new TTree(currFile.getProxy(testTree), currFile);
-    }
 
-    private void testNano19Impl(String branchName, AsDtype.Dtype type) throws IOException {
-        TTree currTree = getTestTree();
-        List<TBranch> branches = currTree.getBranches(branchName);
-        TBranch branch = branches.get(0);
-        List<TBasket> baskets = branch.getBaskets();
-        Cache branchCache = new Cache();
-        SlimTBranch slimBranch = SlimTBranch.getFromTBranch(branch);
-        ArrayBuilder.GetBasket getbasket = slimBranch.getArrayBranchCallback(branchCache);
-        long []basketEntryOffsets = slimBranch.getBasketEntryOffsets();
-        Interpretation interp = new AsJagged(new AsDtype(type));
-        ArrayBuilder builder = new ArrayBuilder(getbasket, interp, basketEntryOffsets, null, 0, 50069);
-        JaggedArrayPrep testarray = (JaggedArrayPrep)builder.getArray(0, 10);
-        System.out.println(Arrays.toString((int[])(testarray.counts().toArray(true))));
-        System.out.println(Arrays.toString((float[])(testarray.content().toArray(true))));
-    }
 
-    @Test
-    public void testNano19_jetPt() throws IOException {
-        testNano19Impl("Jet_pt", AsDtype.Dtype.FLOAT4);
-    }
 
-    @Test
-    public void testNano19_nMuon() throws IOException {
-        testNano19Impl("nMuon", AsDtype.Dtype.INT4);
-    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 }
