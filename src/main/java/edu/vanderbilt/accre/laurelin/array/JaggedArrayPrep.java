spark-root/laurelin

commit 5ba53857c065c0d80889e7fd575639556bf71f18

JaggedArrayPrep.clip was also wrong; fixed it and added a test that reads all Jet_pt from nano_19.root

src/main/java/edu/vanderbilt/accre/laurelin/array/JaggedArrayPrep.java
@@ -42,7 +42,7 @@ public class JaggedArrayPrep extends Array {
 
         int itemstart = this.offsets.get(start);
         int itemstop = this.offsets.get(stop);
-        return this.content.clip(itemstart, itemstop);
+        return new JaggedArrayPrep(this.interpretation, stop - start, (PrimitiveArray.Int4)this.counts.clip(start, stop), this.content.clip(itemstart, itemstop));
     }
 
     public Object toArray(boolean bigEndian) {

src/test/java/edu/vanderbilt/accre/array/ArrayBoundsTest.java
@@ -9,6 +9,24 @@ import edu.vanderbilt.accre.laurelin.array.RawArray;
 import edu.vanderbilt.accre.laurelin.interpretation.AsDtype;
 import edu.vanderbilt.accre.laurelin.interpretation.Interpretation;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+import java.util.Arrays;
+import edu.vanderbilt.accre.laurelin.Cache;
+import edu.vanderbilt.accre.laurelin.array.ArrayBuilder;
+import edu.vanderbilt.accre.laurelin.interpretation.AsDtype;
+import edu.vanderbilt.accre.laurelin.interpretation.AsJagged;
+import edu.vanderbilt.accre.laurelin.interpretation.Interpretation;
+import edu.vanderbilt.accre.laurelin.root_proxy.TBasket;
+import edu.vanderbilt.accre.laurelin.root_proxy.TBranch;
+import edu.vanderbilt.accre.laurelin.root_proxy.TFile;
+import edu.vanderbilt.accre.laurelin.root_proxy.TLeaf;
+import edu.vanderbilt.accre.laurelin.root_proxy.TTree;
+import edu.vanderbilt.accre.laurelin.spark_ttree.SlimTBranch;
+import edu.vanderbilt.accre.laurelin.array.Array;
+import edu.vanderbilt.accre.laurelin.array.JaggedArrayPrep;
+
 public class ArrayBoundsTest {
 
     @Test
@@ -32,6 +50,31 @@ public class ArrayBoundsTest {
         Interpretation interpretation = new AsDtype(AsDtype.Dtype.UINT2);
         ArrayBuilder builder0 = new ArrayBuilder(new DummyBranchCallback(), interpretation, basketEntryOffsets, null, 0, 1000);
         ArrayBuilder builder1 = new ArrayBuilder(new DummyBranchCallback(), interpretation, basketEntryOffsets, null, 1000, 4266);
+    }
+
+    private TTree getTestTree() throws IOException {
+        String testPath = "nano_19.root";
+        String testTree = "Events";
+        File f = new File(testPath);
+        TFile currFile = TFile.getFromFile(testPath);
+        return new TTree(currFile.getProxy(testTree), currFile);
+    }
+
+    @Test
+    public void testNano19() throws IOException {
+        TTree currTree = getTestTree();
+        List<TBranch> branches = currTree.getBranches("Jet_pt");
+        TBranch branch = branches.get(0);
+        List<TBasket> baskets = branch.getBaskets();
+        Cache branchCache = new Cache();
+        SlimTBranch slimBranch = SlimTBranch.getFromTBranch(branch);
+        ArrayBuilder.GetBasket getbasket = slimBranch.getArrayBranchCallback(branchCache);
+        long []basketEntryOffsets = slimBranch.getBasketEntryOffsets();
+        Interpretation interp = new AsJagged(new AsDtype(AsDtype.Dtype.FLOAT4));
+        ArrayBuilder builder = new ArrayBuilder(getbasket, interp, basketEntryOffsets, null, 0, 50069);
+        JaggedArrayPrep testarray = (JaggedArrayPrep)builder.getArray(0, 10);
+        System.out.println(Arrays.toString((int[])(testarray.counts().toArray(true))));
+        System.out.println(Arrays.toString((float[])(testarray.content().toArray(true))));
     }
 
 }

commit 2261d3a214b77c6ae3ea872d44174c1213575070

Fix subarray bug for jagged arrays
Ideally fix the bug introduced with 5ba53857c

src/main/java/edu/vanderbilt/accre/laurelin/array/JaggedArrayPrep.java
@@ -1,10 +1,7 @@
 package edu.vanderbilt.accre.laurelin.array;
 
-import java.lang.UnsupportedOperationException;
 import java.nio.ByteBuffer;
 
-import edu.vanderbilt.accre.laurelin.array.Array;
-import edu.vanderbilt.accre.laurelin.array.PrimitiveArray;
 import edu.vanderbilt.accre.laurelin.interpretation.Interpretation;
 
 public class JaggedArrayPrep extends Array {
@@ -15,8 +12,8 @@ public class JaggedArrayPrep extends Array {
     public JaggedArrayPrep(Interpretation interpretation, int length, PrimitiveArray.Int4 counts, Array content) {
         super(interpretation, length);
         this.counts = counts;
-        this.offsets = null;
         this.content = content;
+        this.offsets = null;
     }
 
     public PrimitiveArray.Int4 counts() {
@@ -27,31 +24,44 @@ public class JaggedArrayPrep extends Array {
         return this.content;
     }
 
+    private void allocateOffsets() {
+        ByteBuffer offsetsbuf = ByteBuffer.allocate((this.counts.length() + 1) * 4);
+        int last = 0;
+        int startpos = this.counts.get(0);
+        offsetsbuf.putInt(last);
+        for (int i = 0; i < this.counts.length(); i++) {
+            last += this.counts.get(i);
+            offsetsbuf.putInt(last);
+        }
+        offsetsbuf.flip();
+        this.offsets = new PrimitiveArray.Int4(new RawArray(offsetsbuf));
+    }
+
+    @Override
     public Array clip(int start, int stop) {
         if (this.offsets == null) {
-            ByteBuffer offsetsbuf = ByteBuffer.allocate((this.counts.length() + 1) * 4);
-            int last = 0;
-            offsetsbuf.putInt(last);
-            for (int i = 0;  i < this.counts.length();  i++) {
-                last += this.counts.get(i);
-                offsetsbuf.putInt(last);
-            }
-            offsetsbuf.position(0);
-            this.offsets = new PrimitiveArray.Int4(new RawArray(offsetsbuf));
+            allocateOffsets();
         }
-
         int itemstart = this.offsets.get(start);
         int itemstop = this.offsets.get(stop);
         return new JaggedArrayPrep(this.interpretation, stop - start, (PrimitiveArray.Int4)this.counts.clip(start, stop), this.content.clip(itemstart, itemstop));
     }
 
+    @Override
     public Object toArray(boolean bigEndian) {
+
         throw new UnsupportedOperationException("not implemented yet");
     }
 
     @Override
     public Array subarray() {
-        throw new UnsupportedOperationException("not implemented yet");
+        if (this.offsets == null) {
+            allocateOffsets();
+        }
+        int low = this.offsets.get(0);
+        int high = this.offsets.get(1);
+        Array ret = this.content.clip(low, high);
+        return ret;
     }
 
 }

src/main/java/edu/vanderbilt/accre/laurelin/array/PrimitiveArray.java
@@ -69,7 +69,7 @@ public abstract class PrimitiveArray extends Array {
         ByteBuffer out = this.buffer.duplicate();
         out.position(bytestart);
         out.limit(bytestop);
-        return this.make(out);
+        return this.make(out.slice());
     }
 
     public RawArray rawarray() {

src/main/java/edu/vanderbilt/accre/laurelin/root_proxy/TBranch.java
@@ -136,20 +136,12 @@ public class TBranch {
     }
 
     public String getFullName() {
-        if (parent == null) {
-            return (String) data.getScalar("fName").getVal();
-        } else {
-            TBranch tmpParent = parent;
-            String ret = "." + getName();
-            while (tmpParent != null) {
-                ret = tmpParent.getName() + ret;
-                tmpParent = tmpParent.parent;
-                if (tmpParent != null) {
-                    ret = "." + ret;
-                }
-            }
-            return ret;
+        String ret;
+        ret = (String) data.getScalar("fName").getVal();
+        if (ret.endsWith(".")) {
+            ret = ret.substring(0, ret.length() - 1);
         }
+        return ret;
     }
 
     public String getClassName() {
@@ -207,7 +199,9 @@ public class TBranch {
      *         null otherwise
      */
     public ArrayDescriptor getArrayDescriptor() {
-        if (getLeaves().size() != 1) {
+        if (getLeaves().size() == 0) {
+            return null;
+        } else  if (getLeaves().size() != 1) {
             throw new RuntimeException("Non-split branches are not supported");
         }
         ArrayDescriptor ret = null;

src/main/java/edu/vanderbilt/accre/laurelin/spark_ttree/TTreeColumnVector.java
@@ -113,9 +113,10 @@ public class TTreeColumnVector extends ColumnVector {
 
     @Override
     public ColumnarArray getArray(int rowId) {
-        Array array = builder.getArray(rowId, 1).subarray();
-        ArrayColumnVector tmpvec = new ArrayColumnVector(((ArrayType)dataType()).elementType(), array);
-        return new ColumnarArray(tmpvec, 0, array.length());
+        Array array = builder.getArray(rowId, 1);
+        Array subarray = array.subarray();
+        ArrayColumnVector tmpvec = new ArrayColumnVector(((ArrayType)dataType()).elementType(), subarray);
+        return new ColumnarArray(tmpvec, 0, subarray.length());
     }
 
     @Override