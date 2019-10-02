7d83f2bcac131bf03fccecf89558969b226b2250
Andrew Melo <andrew.m.melo@vanderbilt.edu>
2019-10-02 14:57:09 -0500

    Do not forcibly close files while they're in use
    The TTree object holds a reference to the backing file, which lets us
    later drill down and request the basket info in a lazy manner. However,
    moving from an eager to lazy initialization for the basket info means
    that we need to keep the open filehandle around longer. Remove the
    try-with-resources to prevent closing the file.

--- src/main/java/edu/vanderbilt/accre/laurelin/Root.java
+++ src/main/java/edu/vanderbilt/accre/laurelin/Root.java
@@ -361,38 +361,41 @@ public class Root implements DataSourceV2, ReadSupport, DataSourceRegister {
                 List<InputPartition<ColumnarBatch>> ret = new ArrayList<InputPartition<ColumnarBatch>>();
                 int pid = 0;
                 TTree inputTree;
-                try (TFile inputFile = TFile.getFromFile(path)) {
+
+                try {
+                    TFile inputFile = TFile.getFromFile(fileCache.getROOTFile(path));
                     inputTree = new TTree(inputFile.getProxy(treeName), inputFile);
+
+                    Map<String, SlimTBranch> slimBranches = new HashMap<String, SlimTBranch>();
+                    parseStructFields(inputTree, slimBranches, schema, "");
+
+
+                    // TODO We partition based on the basketing of the first branch
+                    //      which might not be optimal. We should do something
+                    //      smarter later
+                    long[] entryOffset = inputTree.getBranches().get(0).getBasketEntryOffsets();
+                    for (int i = 0; i < (entryOffset.length - 1); i += 1) {
+                        pid += 1;
+                        long entryStart = entryOffset[i];
+                        long entryEnd = entryOffset[i + 1];
+                        // the last basket is dumb and annoying
+                        if (i == (entryOffset.length - 1)) {
+                            entryEnd = inputTree.getEntries();
+                        }
+
+                        ret.add(new TTreeDataSourceV2Partition(schema, basketCacheFactory, entryStart, entryEnd, slimBranches, threadCount, profileData, pid));
+                    }
+                    if (ret.size() == 0) {
+                        // Only one basket?
+                        logger.debug("Planned for zero baskets, adding a dummy one");
+                        pid += 1;
+                        ret.add(new TTreeDataSourceV2Partition(schema, basketCacheFactory, 0, inputTree.getEntries(), slimBranches, threadCount, profileData, pid));
+                    }
+                    return ret.iterator();
                 } catch (Exception e) {
                     throw new RuntimeException(e);
                 }
 
-                Map<String, SlimTBranch> slimBranches = new HashMap<String, SlimTBranch>();
-                parseStructFields(inputTree, slimBranches, schema, "");
-
-
-                // TODO We partition based on the basketing of the first branch
-                //      which might not be optimal. We should do something
-                //      smarter later
-                long[] entryOffset = inputTree.getBranches().get(0).getBasketEntryOffsets();
-                for (int i = 0; i < (entryOffset.length - 1); i += 1) {
-                    pid += 1;
-                    long entryStart = entryOffset[i];
-                    long entryEnd = entryOffset[i + 1];
-                    // the last basket is dumb and annoying
-                    if (i == (entryOffset.length - 1)) {
-                        entryEnd = inputTree.getEntries();
-                    }
-
-                    ret.add(new TTreeDataSourceV2Partition(schema, basketCacheFactory, entryStart, entryEnd, slimBranches, threadCount, profileData, pid));
-                }
-                if (ret.size() == 0) {
-                    // Only one basket?
-                    logger.debug("Planned for zero baskets, adding a dummy one");
-                    pid += 1;
-                    ret.add(new TTreeDataSourceV2Partition(schema, basketCacheFactory, 0, inputTree.getEntries(), slimBranches, threadCount, profileData, pid));
-                }
-                return ret.iterator();
             }
 
             FlatMapFunction<String, InputPartition<ColumnarBatch>> getLambda() {
