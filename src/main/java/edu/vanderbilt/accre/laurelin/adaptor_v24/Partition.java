35e61ff9cc111aa6d2e9414d0dd18f6485bd863b
Andrew Melo <andrew.m.melo@vanderbilt.edu>
2020-03-06 21:39:51 -0600

    Halfway shift ROOT code to v24 adaptor
    Need to continue splitting the code into the meat and a wrapper around
    the spark v24 api

---
+++ src/main/java/edu/vanderbilt/accre/laurelin/adaptor_v24/Partition.java
@@ -0,0 +1,55 @@
+package edu.vanderbilt.accre.laurelin.adaptor_v24;
+
+import java.util.Map;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.apache.spark.sql.sources.v2.reader.InputPartition;
+import org.apache.spark.sql.sources.v2.reader.InputPartitionReader;
+import org.apache.spark.sql.types.StructType;
+import org.apache.spark.sql.vectorized.ColumnarBatch;
+import org.apache.spark.util.CollectionAccumulator;
+
+import edu.vanderbilt.accre.laurelin.root_proxy.io.IOProfile.Event.Storage;
+import edu.vanderbilt.accre.laurelin.spark_ttree.SlimTBranch;
+import edu.vanderbilt.accre.laurelin.adaptor_v24.PartitionReader;
+
+/**
+ * Represents a Partition of a TTree.
+ *
+ * <p>This is instantiated on the driver, then serialized and transmitted to
+ * the executor
+ */
+class Partition implements InputPartition<ColumnarBatch> {
+    static final Logger logger = LogManager.getLogger();
+
+    private static final long serialVersionUID = -6598704946339913432L;
+    private StructType schema;
+    private long entryStart;
+    private long entryEnd;
+    private Map<String, SlimTBranch> slimBranches;
+    private int threadCount;
+    private CollectionAccumulator<Storage> profileData;
+    private int pid;
+
+    public Partition(StructType schema, long entryStart, long entryEnd, Map<String, SlimTBranch> slimBranches, int threadCount, CollectionAccumulator<Storage> profileData, int pid) {
+        logger.trace("dsv2partition new");
+        this.schema = schema;
+        this.entryStart = entryStart;
+        this.entryEnd = entryEnd;
+        this.slimBranches = slimBranches;
+        this.threadCount = threadCount;
+        this.profileData = profileData;
+        this.pid = pid;
+    }
+
+    @Override
+    public InputPartitionReader<ColumnarBatch> createPartitionReader() {
+        logger.trace("input partition reader");
+        return new PartitionReader(schema, entryStart, entryEnd, slimBranches, threadCount, profileData, pid);
+    }
+
+    public void setPid(int pid) {
+        this.pid = pid;
+    }
+}
